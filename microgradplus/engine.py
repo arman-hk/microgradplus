1ad76604d635dc572f2879f86f904477e479f027
arman-hk <armanhku@gmail.com>
2023-05-23 19:50:35 +0330

    add _repr_ and _add_

--- microgradplus/engine.py
+++ microgradplus/engine.py
@@ -7,6 +7,20 @@ class Value:
         self.data = np.array(data)
         self.grad = np.zeros_like(self.data)
         # internal variables
-        self._backward = lambda: None
         self._prev = set(_children)  # stores previous objects
         self._grad_fn = _grad_fn  # stores the gradient function
+    
+    def __add__(self, other):
+        # if `other` is a scalar, convert it to a `Value`
+        if isinstance(other, (int, float)):
+            other = Value(other)
+        
+        def _grad_fn():
+            self.grad += 1 * out.grad
+            other.grad += 1 * out.grad
+        
+        out = Value(self.data + other.data, (self, other), _grad_fn)
+        return out
+        
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
