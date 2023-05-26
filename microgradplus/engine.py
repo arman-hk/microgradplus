36e1762f5f1d98c26feb2e3d3b5ab6af5ac65793
arman-hk <armanhku@gmail.com>
2023-05-26 19:10:27 +0330

    fixed backward pass

--- microgradplus/engine.py
+++ microgradplus/engine.py
@@ -26,13 +26,22 @@ class Value:
         ctx = Context()
         ctx.save_for_backward(self.data, other.data)
         
-        def _grad_fn():
+        def _grad_fn(grad):
             self_data, other_data = ctx.saved_arrays.values()
-            self.grad += 1 * out.grad
-            other.grad += 1 * out.grad
+            self.grad += grad
+            other.grad += grad
     
         out = Value(self.data + other.data, (self, other), _grad_fn)
         return out
-        
+
+    def backward(self, grad=None):
+        if grad is None:
+            grad = np.ones_like(self.data)
+        self.grad = grad
+        if self._grad_fn is not None:
+            self._grad_fn(grad)
+        for child in self._prev:
+            child.backward(grad)
+ 
     def __repr__(self):
         return f"Value(data={self.data}, grad={self.grad})"
