0204750a5b1ead8f7b8f787b32486693368da238
arman-hk <armanhku@gmail.com>
2023-05-28 20:31:25 +0330

    power function test'

--- microgradplus/test.py
+++ microgradplus/test.py
@@ -1,8 +1,8 @@
 from engine import Value
-
-a = Value([1, 2, 3])
-b = Value([4, 5, 6])
-c = a * b
+#testing pow with large numbers
+a = Value([1e4, 1e5, 1e6])
+b = Value([2.0, 2.5, 3.0])
+c = a ** b
 c.backward()
 print(a)
 print(b)
