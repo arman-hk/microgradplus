41719bcd9c81929f85451aa0f95ce7e71a834624
arman-hk <armanhku@gmail.com>
2023-05-28 20:30:11 +0330

    power function

--- microgradplus/engine.py
+++ microgradplus/engine.py
@@ -15,7 +15,7 @@ class Value:
     
     def __init__(self, data, _children=(), _grad_fn=None):
         self.data = np.array(data)
-        self.grad = np.zeros_like(data)
+        self.grad = np.zeros_like(data, dtype=float)
         # internal variables
         self._prev = set(_children)
         self._grad_fn = _grad_fn
@@ -42,6 +42,18 @@ class Value:
             self.grad += other_data * grad
             other.grad += self_data * grad
         out = Value(self.data * other.data, (self, other), _grad_fn)
+        return out
+
+    def __pow__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        ctx = Context()
+        ctx.save_for_backward(self.data, other.data)
+
+        def _grad_fn(grad):
+            self_data, other_data = ctx.saved_arrays.values()
+            self.grad += (other_data * self_data ** (other_data - 1)) * grad
+            other.grad += np.sum((self.data ** other_data * np.log(self_data)) * grad)
+        out = Value(self.data ** other.data, (self, other), _grad_fn)
         return out
     
     def backward(self, grad=None):
