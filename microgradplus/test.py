f3834ca265369d99819f4636ed21e8418ab39551
arman-hk <armanhku@gmail.com>
2023-06-01 18:51:50 +0330

    sqrt test

--- microgradplus/test.py
+++ microgradplus/test.py
@@ -1,10 +1,6 @@
 from engine import Value
-
-a = Value([4.0, 10.0, 12.0])
-b = Value([2.0, 5.0, 6.0])
-c = a / b
+a = Value([1, 2, 3])
+c = Value.sqrt(a)
 c.backward()
-
 print(f"a = {a}")
-print(f"b = {b}")
 print(f"c = a/b = {c}")
