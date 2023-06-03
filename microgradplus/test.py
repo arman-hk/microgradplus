e9deadae40b4be9a8f63562587c8417a610bb052
arman-hk <armanhku@gmail.com>
2023-06-03 12:36:24 +0330

    testing

--- microgradplus/test.py
+++ microgradplus/test.py
@@ -1,8 +1,27 @@
 from engine import Value
 
-a = Value([-1, 0, 1, 2, 3])
-c = Value.relu(a)
-c.backward()
+a = Value(-2)
+b = Value(3)
+c = a + b
+d = c * b
+e = d / a
+f = e - b
+g = f ** 2
+h = g.exp()
+i = Value.sqrt(h)
+j = i.abs()
+k = Value.relu(j)
+
+k.backward()
 
 print(f"a = {a}")
-print(f"c = relu(a) = {c}")
+print(f"b = {b}")
+print(f"c = {c}")
+print(f"d = {d}")
+print(f"e = {e}")
+print(f"f = {f}")
+print(f"g = {g}")
+print(f"h = {h}")
+print(f"i = {i}")
+print(f"j = {j}")
+print(f"k = {k}")
