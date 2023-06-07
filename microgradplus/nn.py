46d8376b7a5be6b8ace2c892cb76c73db2b031f7
arman-hk <armanhku@gmail.com>
2023-06-07 17:12:38 +0330

    badass linear class

---
+++ microgradplus/nn.py
@@ -0,0 +1,19 @@
+import numpy as np
+from engine import Value
+
+class Linear:
+    def __init__(self, input_dim, output_dim):
+        # init weights and biases
+        self.weights = Value(np.random.randn(input_dim, output_dim) * 0.01)
+        self.bias = Value(np.zeros(output_dim))
+
+    def __call__(self, x):
+        # forward pass
+        self.x = x
+        return x @ self.weights + self.bias
+
+    def backward(self, grad):
+        # grads with respect to inputs and params
+        self.weights.grad += self.x.T @ grad
+        self.bias.grad += np.sum(grad, axis=0)
+        return grad @ self.weights.data.T
